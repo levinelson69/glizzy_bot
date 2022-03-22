1fc07e61ac413eef051777a98458c91f996fe860
levinelson69 <99291501+levinelson69@users.noreply.github.com>
2022-03-23 10:00:08 +1300

    Created wel_mesv1
    Printed a welcome message and generated a list of 10 random names

---
+++ Components/wel_mesv1.py
@@ -0,0 +1,14 @@
+# Glizzy bot program
+import random
+from random import randint
+
+#List of random names
+names = ["Mickey", "Nate", "Jay", "Jess", "Sally", "Annie", "Christopher", "Jake", "Ochre", "Minty"]
+
+num = randint(0,9)
+
+name = (names[num])
+
+print("*** Welcome to Mickeys Glizzys ***")
+print("*** My name is",name, "***")
+print("I will be here to help you to order your hot and ready Sausages")
