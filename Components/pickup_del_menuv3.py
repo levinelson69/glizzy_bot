19d68975e019a9aacd531687a76e652bfd8fde29
levinelson69 <99291501+levinelson69@users.noreply.github.com>
2022-03-29 11:54:55 +1300

    Created component 2
    Created a menu to ask the user if they would like their order for pickup or if they want it delivered.

--- Components/pickup_del_menuv3.py
+++ Components/pickup_del_menuv3.py
@@ -1,5 +1,8 @@
+# Bugs
+#Will only work for valid input "1" and "2"
+
 # Menu so that the user can choose either pickup or delivery
-#Bug: Must make program so it only accepts 1 or 2
+#When input is invalid, else statement is triggered but program does not ask for another input
 
 print ("Is your order for Pickup or would you like it for Delivery?")
 
@@ -7,22 +10,18 @@ print ("For delivery please enter 1")
 print ("For pickup please enter 2")
 
 
-low = 1
-high = 2
 
-while True:
-    try:
-        delivery =int(input("Please enter a number"))
+delivery =int(input())
 
-        if delivery == 1:
-            print ("Pickup")
-            break          
+if delivery == "1":
+    print ("Delivery")
+         
 
-        elif delivery == 2:
-            print ("Delivery")
-            break
+elif delivery == "2":
+    print ("Pickup")
+
+else:
+   print ("**That was not a valid input**")
+
     
-    except ValueError:
-        print("**That is not a valid number**")
-        print("Please enter 1 for Pickup or 2 for Delivery")
 
