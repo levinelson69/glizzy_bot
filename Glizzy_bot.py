66e9a142b4eb23678e31c414047dd1c04daeb321
levinelson69 <99291501+levinelson69@users.noreply.github.com>
2022-03-29 12:12:43 +1300

    integrated pickup_del menu
    Integrated pickup_del menu into main code

--- Glizzy_bot.py
+++ Glizzy_bot.py
@@ -35,6 +35,42 @@ print("I will be here to help you to order your hot and ready Sausages")
 
 
 
+# Menu so that the user can choose either pickup or delivery
+print ("Is your order for Pickup or would you like it for Delivery?")
+print ("For Pickup please enter 1")
+print ("For Delivery please enter 2")
+while True:
+    try:
+        delivery = int(input("Please enter a number "))
+        if delivery >= 1 and delivery <= 2:
+            if delivery == 1:
+                print ("Pickup")
+                break
+
+            elif delivery == 2:
+                print ("Delivery")
+                break
+        else:
+            print("Number must be 1 or 2")
+    except ValueError:
+        print("That is not a valid number")
+        print("Please enter 1 or 2")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
