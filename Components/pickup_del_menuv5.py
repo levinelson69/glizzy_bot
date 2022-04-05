1514f26672139658193997c467132d1bdb6817f4
levinelson69 <99291501+levinelson69@users.noreply.github.com>
2022-04-06 10:00:23 +1200

    Created comments and pickup v4
    Wrote comments and did pickup v4 component

--- Components/pickup_del_menuv5.py
+++ Components/pickup_del_menuv5.py
@@ -1,20 +1,22 @@
 # Menu so that the user can choose either pickup or delivery
-print ("Is your order for Pickup or would you like it for Delivery?")
-print ("For Pickup please enter 1")
-print ("For Delivery please enter 2")
-while True:
-    try:
-        delivery = int(input("Please enter a number "))
-        if delivery >= 1 and delivery <= 2:
-            if delivery == 1:
+# Parameters: none
+# Returns: none
+print ("Is your order for Pickup or would you like it for Delivery?") #Asks the user if they would like thier order for pickup or delivery
+print ("For Pickup please enter 1") #If the user would like pickup, they must input 1
+print ("For Delivery please enter 2") #If the user would like delivery, they must input 2
+while True: # Sets up while loop
+    try: #checking the code for errors
+        delivery = int(input("Please enter a number ")) # Input for the user to enter a number between 1 or 2
+        if delivery >= 1 and delivery <= 2: #If statement for if the input is between 1 and 2
+            if delivery == 1: #If 1 is entered print Pickup statement
                 print ("Pickup")
-                break
+                break # To break out of the loop
 
-            elif delivery == 2:
+            elif delivery == 2: #If 2 is entered print Delivery statement
                 print ("Delivery")
-                break
-        else:
+                break #To break out of the loop
+        else: #If the integer input is not a 1 or a 2, print error statement
             print("Number must be 1 or 2")
-    except ValueError:
+    except ValueError: #If input is not a number, print error statement
         print("That is not a valid number")
         print("Please enter 1 or 2")
--- Components/wel_mesv2.py
+++ Components/wel_mesv2.py
@@ -1,11 +1,13 @@
 # Glizzy bot program
-import random
-from random import randint
+# Parameters: None
+# Returns: None
+import random #Importing a random ___
+from random import randint #Importing a random number from randint variable
 
-#List of random names
+#List of random names with 'names' variable
 names = ["Mickey", "Nate", "Jay", "Jess", "Sally", "Annie", "Christopher", "Jake", "Ironone", "Betty"]
 
-def welcome():
+def welcome(): #defining welcome function to generate a random name from list and to print the welcome message
     '''
     Purpose: To generate a random name from the list and print out a welcome message
     Parameters: none
@@ -20,12 +22,12 @@ print("*** Welcome to Mickeys Glizzys ***")
 print("*** My name is",name, "***")
 print("I will be here to help you to order your hot and ready Sausages")
 
-def main():
+def main(): #defining the main function for my program
     '''
     Purpose: To generate a random name from the list and print out a welcome message
     Parameters: none
     Returns: none
     '''
-    welcome()
+    welcome() 
 
-main()
+main() #main function used in my program
